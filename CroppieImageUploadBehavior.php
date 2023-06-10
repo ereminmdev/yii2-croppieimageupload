4c75c42cd8a7cd849778008fa5fbbdb2e33180a8
Eremin Mikhail <ereminmdev@gmail.com>
2023-06-10 12:21:24 +0500

    Improve finding uploaded file as data:image or base64

--- CroppieImageUploadBehavior.php
+++ CroppieImageUploadBehavior.php
@@ -152,9 +152,9 @@ class CroppieImageUploadBehavior extends UploadImageBehavior
     {
         $value = $this->crop_value;
 
-        if (mb_strpos($value, 'action=') === 0) {
+        if (mb_stripos($value, 'action=') === 0) {
             $this->action = mb_substr($value, 7);
-        } elseif ((mb_strpos($value, 'data:image') === 0) && mb_strpos($value, 'base64,')) {
+        } elseif ((mb_stripos($value, 'data:image') === 0) && mb_stripos($value, 'base64,')) {
             $this->createFromBase64($value);
         } elseif (filter_var($value, FILTER_VALIDATE_URL)) {
             $this->createFromUrl($value);
